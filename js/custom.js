d3f93bd3552f04941021b062d93de0102a0c1c65
Thomas Schertenleib <thomas.schertenleib@gmail.com>
2016-10-17 17:00:00 +0200

    scroll to function done

--- js/custom.js
+++ js/custom.js
@@ -3,63 +3,18 @@ $(document).ready(function(){
 
   // scroll to position
 
+var y = $(window).scrollTop();  //your current y position on the page
 
-  // navigation
-  $(".scroll-start").click(function() {
+
+  $(".scroll").click(function() {
       $('html, body').animate({
-          scrollTop: $(".error-page").offset().top
-      }, 1000);
+          scrollTop: $(".weiss").offset().top-122
+      }, 500);
   });
 
-  $(".scroll-projects").click(function() {
-      $('html, body').animate({
-          scrollTop: $(".projects-gallery").offset().top
-      }, 1000);
-  });
-
-  $(".scroll-about").click(function() {
-      $('html, body').animate({
-          scrollTop: $(".about-thomas").offset().top
-      }, 1000);
-  });
-
-  $(".scroll-contact").click(function() {
-      $('html, body').animate({
-          scrollTop: $(".contact-photo").offset().top
-      }, 1000);
-  });
-
-  // buttons
-  $(".one").click(function() {
-      $('html, body').animate({
-          scrollTop: $(".scroll-gallery").offset().top
-      }, 600);
-  });
-
-  $(".two").click(function() {
-      $('html, body').animate({
-          scrollTop: $(".contact-photo").offset().top
-      }, 1300);
-  });
-
-
-  // $('.one').click(function() {
-  //   $('.scroll-gallery').scrollView();
-  // });
-
-// fullscreen video
 
 
 
 });
 
 
-
-// $(function(){
-//   $('#video').css({ width: $(window).innerWidth() + 'px', height: $(window).innerHeight() + 'px' });
-//
-//   // If you want to keep full screen on window resize
-//   $(window).resize(function(){
-//     $('#video').css({ width: $(window).innerWidth() + 'px', height: $(window).innerHeight() + 'px' });
-//   });
-// });
