e5f6441a48ef056bf3ed0f93038ebb6c24f0b059
Thomas Schertenleib <thomas.schertenleib@gmail.com>
2016-10-18 14:12:48 +0200

    header scroll on click responsive

--- js/custom.js
+++ js/custom.js
@@ -5,14 +5,22 @@ $(document).ready(function(){
 
 var y = $(window).scrollTop();  //your current y position on the page
 
+$(window).on("resize", function(event){
+  console.log( $(this).width() );
+});
 
   $(".scroll").click(function() {
+if ($(window).width() > 752) {
       $('html, body').animate({
           scrollTop: $(".weiss").offset().top-122
       }, 500);
+ } else {
+      $('html, body').animate({
+          scrollTop: $(".weiss").offset().top-80
+      }, 500);
+}    
+
   });
-
-
 
 
 });
